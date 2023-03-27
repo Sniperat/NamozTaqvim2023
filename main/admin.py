e07b7aa9993c1ef7ba9cac1e0023a173f1ee6a70
asd <asd@asd.asd>
2023-03-28 00:10:24 +0500

    edit list display

--- main/admin.py
+++ main/admin.py
@@ -2,7 +2,10 @@ from django.contrib import admin
 from .models import TelegramUserModel, RegionModel, TimeModel, QazoModel
 
 
-admin.site.register(TelegramUserModel)
+class DetailAdmin(admin.ModelAdmin):
+    list_display = ('chat_id', 'username', 'is_active',)
+    
+admin.site.register(TelegramUserModel, DetailAdmin)
 admin.site.register(RegionModel)
 admin.site.register(TimeModel)
 admin.site.register(QazoModel)
